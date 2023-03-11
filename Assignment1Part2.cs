1f7a04639d61d382fb55928ae623c401ee238a17
sms0157 <127634473+sms0157@users.noreply.github.com>
2023-03-11 18:38:10 -0500

    Add files via upload

---
+++ Assignment1Part2.cs
@@ -0,0 +1,38 @@
+﻿namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result;
+
+            Console.Write("Enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("1 (add), 2 (subtract), 3 (multiply)");
+            Console.Write("Enter number option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            if (option == 1)
+            {
+                result = num1 + num2;
+                Console.WriteLine("Sum of numbers: " + result.ToString());
+            }
+            else if (option == 2)
+            {
+                result = num1 - num2;
+                Console.WriteLine("Difference of numbers: " + result.ToString());
+            }
+            else if (option == 3)
+            {
+                result = num1 * num2;
+                Console.WriteLine("Product of numbers: " + result.ToString());
+            }
+            else
+                Console.Write("Invalid choice.");
+            Console.ReadLine();
+        }
+    }
+}
